c9628c01bb558a17de4c634938b7e0c53c681f41
Rajarshi Ganguly <rajarshiganguly@Rajus-iMac.local>
2013-10-30 19:35:59 +0530

    minor changes

--- scripts/application.js
+++ scripts/application.js
@@ -1,5 +1,7 @@
 $("#read_more").click(function(){
-    $(this).toggleClass("active");
-    $(this).next("div").stop('true','true').slideToggle("slow");
+    // $(this).toggleClass("active");
+    $(this).next("div").stop('true','true').slideToggle("slow");   
+    $("#read_more_wrapper").toggleClass("hide");
+    $("#read_more_wrapper").hasClass("hide") ? $("#read_more").text("Read more about me +") : $("#read_more").text("Read more about me -");
 });
   
