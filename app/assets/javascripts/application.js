90674d98cecc833e2f858e1402ab4f6cdee3e18d
Rajarshi Ganguly <rajarshiganguly@Rajus-iMac.local>
2013-11-01 19:12:40 +0530

    file inclusion fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require_tree .
 
 
-
-$("#read_more").click(function(){
-    // $(this).toggleClass("active");
-    $(this).next("div").stop('true','true').slideToggle("slow");   
-    $("#read_more_wrapper").toggleClass("hide");
-    $("#read_more_wrapper").hasClass("hide") ? $("#read_more").text("Read more about me +") : $("#read_more").text("Read more about me -");
-});
